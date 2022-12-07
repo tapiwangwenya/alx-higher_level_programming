c462cd44e7d360c2c7223e1600a7cd81a866cbf9
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-12-07 12:59:33 -0800

    Another update on more structures2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
+search_replace = __import__('1-search_replace').search_replace
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
